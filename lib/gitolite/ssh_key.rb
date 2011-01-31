84c2a839e6d9cdd688a0ecc0a490ef5e31fa0142
Stafford Brunk <wingrunr21@gmail.com>
2011-01-30 21:26:22 -0500

    Major changes to SSHKey object.

--- lib/gitolite/ssh_key.rb
+++ lib/gitolite/ssh_key.rb
@@ -11,24 +11,6 @@ module Gitolite
   class SSHKey
     attr_accessor :owner, :location, :type, :blob, :email
 
-    def initialize(key)
-
-      raise "#{key} does not exist!" unless File.exists?(key)
-
-      #Get our owner and location
-      File.basename(key) =~ /^(\w+(?:@(?:\w+\.)+\D{2,4})?)(?:@(\w+))?.pub$/i
-      @owner = $1
-      @location = $2 || ""
-
-      #Get parts of the key
-      @type, @blob, @email = File.read(key).split
-
-      #If the key didn't have an email, just use the owner
-      if @email.nil?
-        @email = @owner
-      end
-    end
-    
     def initialize(type, blob, email, owner = nil, location = "")
       @type = type
       @blob = blob
@@ -38,14 +20,48 @@ module Gitolite
       @location = location
     end
 
+    def self.from_file(key)
+
+      raise "#{key} does not exist!" unless File.exists?(key)
+
+      #Get our owner and location
+      File.basename(key) =~ /^(\w+(?:@(?:\w+\.)+\D{2,4})?)(?:@(\w+))?.pub$/i
+      owner = $1
+      location = $2 || ""
+
+      #Get parts of the key
+      type, blob, email = File.read(key).split
+
+      #If the key didn't have an email, just use the owner
+      if email.nil?
+        email = owner
+      end
+      
+      self.new(type, blob, email, owner, location)
+    end
+
     def to_s
       [@type, @blob, @email].join(' ')
     end
     
+    def to_file(filename)
+      File.open(filename, "w") do |f|
+        f.write (self.to_s)
+      end
+    end
+    
     def filename
       file = @owner
-      file << "@#{@location}" unless @location.empty?
-      file << ".pub"
+      file += "@#{@location}" unless @location.empty?
+      file += ".pub"
+    end
+    
+    def ==(key)
+      @type == key.type &&
+      @blob == key.blob &&
+      @email == key.email &&
+      @owner == key.owner &&
+      @location == key.location
     end
   end
 end
